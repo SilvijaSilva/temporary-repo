6f47c3c7350c025441a8c3baa54d78c151436384
SilvijaSilva <silvija.klejevskyte@gmail.com>
2025-05-24 20:28:37 +0200

    Todo list extended with handling for invalid date input

--- python-dictionary-objects/todo_list.py
+++ python-dictionary-objects/todo_list.py
@@ -1,34 +1,17 @@
-# person_list = []
-
-# while True:
-#     person_name = input("Enter person's name: ")
-#     person_age = input("Enter person's age: ")
-#     person_city = input("Enter person's city: ")
-
-#     person = {
-#         'name': person_name,
-#         'age': person_age,
-#         'city': person_city
-#     }
-
-#     person_list.append(person)
-
-#     for p in person_list:
-#         print(f"Name: {p['name']}, Age: {p['age']}, City: {p['city']}")
-
 from datetime import datetime
 
 todo_items_list = []
 
 while True:
     todo_item_title = input("Enter todo's item: ")
-    todo_item_date = str(input("Enter todo's date (YYYY-MM-DD): "))
-
-    try:
-        todo_item_date = datetime.strptime(todo_item_date, "%Y-%m-%d").date()
-    except ValueError:
-        print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
-        continue
+    
+    while True:
+        todo_item_date = (input("Enter todo's date (YYYY-MM-DD): "))
+        try:
+            todo_item_date = datetime.strptime(todo_item_date, "%Y-%m-%d").date()
+            break
+        except ValueError:
+            print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
 
     todo_item = {
         'title': todo_item_title,
@@ -38,4 +21,4 @@ while True:
     todo_items_list.append(todo_item)
 
     for n in todo_items_list:
-        print(f"Title: {n['title']}, Date: {n['date']}")
+        print(f"Work to do: {n['title']}, Date: {n['date']}")
